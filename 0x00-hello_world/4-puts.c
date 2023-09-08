9fa87e8b80829fac599664421899e9e1f1d7d943
Wealth-King <sackeykkojo@gmail.com>
2023-09-07 19:38:27 -0700

    My first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Using the puts function to print the message
+    puts("Programming is like building a multilingual puzzle");
+
+    // Ending the program with the value 0
+    return 0;
+}
