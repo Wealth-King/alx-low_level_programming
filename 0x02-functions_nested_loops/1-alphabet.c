bb181977a4997be6790c125116b9e3298d176e11
Wealth-King <sackeykkojo@gmail.com>
2023-09-12 20:58:39 -0700

    My first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet _ Mkae the alpahabet 
+ * Retuen: void 
+ */
+
+void print_alpahbet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * _islower - CHecks for lowercase character
+ * @c: The character to be checked 
+ * REturn: 1 for lowercase character or 0 for anything else
+ */
+
+int _islower(int c)
+
+{
+
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
