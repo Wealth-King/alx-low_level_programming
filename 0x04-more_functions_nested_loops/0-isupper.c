7053e680aff34db4d4d468f5dae97687ab778ff6
Wealth-King <sackeykkojo@gmail.com>
2023-09-14 17:01:10 -0700

    My first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper = CHecl if the letter is upper
+ * @x: the number to be checked
+ * Returm: 1 for upper letter or 0 for amy else 
+ */
+
+int _isupper(int x)
+
+{
+	if (x >= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0)'
+}
